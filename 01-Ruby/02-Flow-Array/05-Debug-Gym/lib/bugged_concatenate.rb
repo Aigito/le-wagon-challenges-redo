1a5668ea34361f65bb3cbb72e9625e73d2bba5b4
Dylan Pee <peedylan94@gmail.com>
2022-03-04 16:18:05 +1030

    Completed challenge

--- 01-Ruby/02-Flow-Array/05-Debug-Gym/lib/bugged_concatenate.rb
+++ 01-Ruby/02-Flow-Array/05-Debug-Gym/lib/bugged_concatenate.rb
@@ -1,11 +1,11 @@
-def bugged_concatenate(array)
-  concatenated_string = ""
-  array.each do |element|
-    concatenated_string = concatenated_string + element
-  end
+def bugged_concatenate(element)
+  concatenated_string = "1984 "
+
+  concatenated_string = concatenated_string + element
+
   return concatenated_string.upcase
 end
 
 def build_1984_title
-  bugged_concatenate(1, "9", 84, " ", "George Orwell")
+  bugged_concatenate("George Orwell")
 end

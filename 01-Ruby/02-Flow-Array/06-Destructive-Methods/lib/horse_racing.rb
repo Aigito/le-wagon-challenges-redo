94b6dc42ee550ebce50e09bea0b2aa8c457b0e87
Dylan Pee <peedylan94@gmail.com>
2022-03-05 10:14:01 +1030

    Challenge completed

--- 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
+++ 01-Ruby/02-Flow-Array/06-Destructive-Methods/lib/horse_racing.rb
@@ -1,7 +1,7 @@
 def horse_racing_format!(race_array)
   # TODO: modify the given array so that it is horse racing consistent. This method should return nil.
   race_array.each_with_index do |racer, index|
-    race_array[index] = "#{index + 1}-" + racer + "!"
+    race_array[index] = "#{index + 1}-#{racer}!"
   end
 
   race_array.reverse!
